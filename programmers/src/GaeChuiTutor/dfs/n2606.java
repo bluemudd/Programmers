658c7e0ad0a8f58a3c838a8c222174649db83310
muddie <parkjm0817@gmail.com>
2024-10-29 01:25:38 +0900

    백준 DFS 2606 수정

--- programmers/src/GaeChuiTutor/dfs/n2606.java
+++ programmers/src/GaeChuiTutor/dfs/n2606.java
@@ -8,31 +8,36 @@ public class n2606 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int computer = Integer.parseInt(br.readLine());
-        boolean [][] computerList = new boolean[computer][computer];
-        visited = new boolean[computer];
+        ArrayList<Integer> [] computerList = new ArrayList[computer+1];
+        for(int i =0; i<computerList.length; i++){
+            computerList[i] = new ArrayList<>();
+        }
+        visited = new boolean[computer+1];
         int connectCount = Integer.parseInt(br.readLine());
         StringTokenizer st;
         for(int i=0; i<connectCount; i++){
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            computerList[from-1][to-1] = true;
+            computerList[from].add(to);
+            computerList[to].add(from);
         }
         dfs(1, computerList);
         int count = 0;
-        for(int i =0; i<visited.length; i++){
-            if(visited[i]==true){
+        for (boolean b : visited) {
+            if (b) {
                 count++;
             }
         }
         System.out.println(count-1);
     }
-    public static void dfs(int start, boolean[][] list){
-        visited[start-1] = true;
-        for(int i=0; i<list.length; i++){
-            if(list[start-1][i]){
-                dfs(i+1, list);
+    public static void dfs(int start, ArrayList<Integer> [] computerList){
+        visited[start] = true;
+        for(int i=0; i<computerList[start].size(); i++){
+            if(!visited[computerList[start].get(i)]){
+                dfs(computerList[start].get(i), computerList);
             }
+
         }
 
     }
