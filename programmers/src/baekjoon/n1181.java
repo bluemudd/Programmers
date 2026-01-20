753fd99b033dc85618d3e4a71d5eeba086fc044d
muddie <parkjm0817@gmail.com>
2026-01-20 23:48:23 +0900

    26/01/20 백준 1181

---
+++ programmers/src/baekjoon/n1181.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+
+public class n1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String answer = br.readLine();
+        int N = Integer.parseInt(answer);
+        Set<String> set = new HashSet<>();
+
+        for(int i = 0; i< N; i++){
+
+            set.add(br.readLine());
+
+        }
+
+        List<String> words = new ArrayList<>(set);
+
+        words.sort(Comparator.comparing(String::length)
+                .thenComparing(Comparator.naturalOrder()));
+
+        for(int i = 0; i< words.size(); i++){
+            System.out.println(words.get(i));
+        }
+
+    }
+}
